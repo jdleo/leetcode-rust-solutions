90f735c93d7c61a29783932d613aab6fa0575e8a
John Leonardo <j@jdleo.me>
2021-11-14 23:24:34 -0800

    done with 771

---
+++ solutions/771/main.rs
@@ -0,0 +1,21 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn num_jewels_in_stones(jewels: String, stones: String) -> i32 {
+        let mut res: i32 = 0;
+
+        // add all jewels to hash set
+        let mut jewels_set: HashSet<char> = HashSet::new();
+        for j in jewels.chars() {
+            jewels_set.insert(j);
+        }
+
+        // go through each stone
+        for s in stones.chars() {
+            // increment res if its a jewel
+            res += (jewels_set.contains(&s) as i32);
+        }
+
+        res
+    }
+}
