10c259a08487e6c62e30b99561d87cd9eca37681
John Leonardo <j@jdleo.me>
2021-11-14 19:33:26 -0800

    1920

---
+++ solutions/1920/main.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::new();
+
+        for &num in nums.iter() {
+            res.push(nums[num as usize]);
+        }
+
+        res
+    }
+}
