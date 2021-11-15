60b474b50855bca05ed319aa921ca9da773a1996
John Leonardo <j@jdleo.me>
2021-11-14 23:01:30 -0800

    done with 1470

---
+++ solutions/1470/main.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let mut res: Vec<i32> = vec![0; (2 * n) as usize];
+
+        // go up to n
+        for i in 0..n {
+            res[(2 * i) as usize] = nums[i as usize];
+            res[(2 * i + 1) as usize] = nums[(n + i) as usize];
+        }
+
+        res
+    }
+}
