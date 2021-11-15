076990ddcf252396c246c5a093728d3105cdd7f5
John Leonardo <j@jdleo.me>
2021-11-14 19:51:29 -0800

    1480

---
+++ solutions/1480/main.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut sum: i32 = 0;
+
+        nums.iter().map( |num| {
+            sum += num
+            sum
+        }).collect()
+    }
+}
