0c72e55eb1d4dcbfd174b15812416d652fe4ff18
John Leonardo <j@jdleo.me>
2021-11-14 22:48:34 -0800

    add scripts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+use std::env;
+use std::process::Command;
+
+fn new(problem_id: String) {
+    // format mkdir, touch and git commands
+    let mkdir = format!("mkdir solutions/{}", problem_id);
+    let touch = format!("touch solutions/{}/main.rs", problem_id);
+    let git = format!(
+        "git add solutions/{}/main.rs && git commit -m \"create {}\"",
+        problem_id, problem_id
+    );
+
+    // make folder under /solutions
+    Command::new("sh")
+        .arg("-c")
+        .arg(mkdir)
+        .spawn()
+        .expect("error")
+        .wait()
+        .expect("error");
+
+    // create main.rs
+    Command::new("sh")
+        .arg("-c")
+        .arg(touch)
+        .spawn()
+        .expect("error")
+        .wait()
+        .expect("error");
+
+    // commit
+    Command::new("sh")
+        .arg("-c")
+        .arg(git)
+        .spawn()
+        .expect("error")
+        .wait()
+        .expect("error");
+}
+
+fn done(problem_id: String) {
+    // format git command
+    let git = format!(
+        "git add solutions/{}/main.rs && git commit -m \"done with {}\"",
+        problem_id, problem_id
+    );
+
+    // create main.rs
+    Command::new("sh")
+        .arg("-c")
+        .arg(git)
+        .spawn()
+        .expect("error")
+        .wait()
+        .expect("error");
+}
+
 fn main() {
-    println!("Hello, world!");
+    // get args
+    let args: Vec<String> = env::args().collect();
+    let cmd = &args[1];
+    let problem_id = &args[2];
+
+    // figure out which command we want
+    match &cmd[..] {
+        "new" => new(String::from(problem_id)),
+        "done" => done(String::from(problem_id)),
+        _ => {
+            eprintln!("Invalid command...")
+        }
+    }
 }
