12e49d74b8462b0de678caaf9fc4e37c69c32e11
John Leonardo <j@jdleo.me>
2021-11-14 19:56:53 -0800

    1108

---
+++ solutions/1108/main.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn defang_i_paddr(address: String) -> String {
+        let mut res = String::from("");
+
+        for chr in address.chars() {
+            match chr == '.' {
+                true => res.push_str("[.]"),
+                false => res.push(chr),
+            }
+        }
+
+        res
+    }
+}
