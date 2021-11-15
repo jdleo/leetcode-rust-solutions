c348e4e2771f59d8ef1392e1c71efed7559010ca
John Leonardo <j@jdleo.me>
2021-11-14 19:37:06 -0800

    1929

---
+++ solutions/1929/main.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::new();
+
+        // go through nums twice
+        for i in 0..nums.len() * 2 {
+            res.push(nums[(i % nums.len()) as usize]);
+        }
+
+        res
+    }
+}
