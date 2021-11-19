8299c31e0f7a4ef29f2d13b8eec64d8f7accb734
John Leonardo <j@jdleo.me>
2021-11-18 21:22:11 -0800

    done with 1342

---
+++ solutions/1342/main.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn number_of_steps(num: i32) -> i32 {
+        let mut x = num;
+        let mut res: i32 = 0;
+
+        if x == 0 {
+            return 0;
+        }
+
+        // go until num is 0 (we keep reducing it)
+        while x > 0 {
+            // if it's odd, that's one extra step
+            res += 1 + (x & 1);
+            x >>= 1;
+        }
+
+        res - 1
+    }
+}
