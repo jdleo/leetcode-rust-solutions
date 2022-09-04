9eb86168825cac064be59fcd89aaafdd0f07ca37
John Leonardo <j@jdleo.me>
2022-09-04 15:58:31 -0700

    done with 1773

---
+++ solutions/1773/main.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
+        // figure out rule key index (to check items)
+        let mut rule_key_index: usize = 0;
+        match rule_key.as_str() {
+            "type" => rule_key_index = 0,
+            "color" => rule_key_index = 1,
+            _ => rule_key_index = 2,
+        }
+
+        // count items that match
+        items
+            .iter()
+            .filter(|item| item[rule_key_index] == rule_value)
+            .count() as i32
+    }
+}
