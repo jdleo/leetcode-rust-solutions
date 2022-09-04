d96948062b0918132b10ac4d5d1357e6b355deca
John Leonardo <j@jdleo.me>
2022-09-04 13:09:58 -0700

    done with 2114

---
+++ solutions/2114/main.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn most_words_found(sentences: Vec<String>) -> i32 {
+        // 1. convert to iterator
+        // 2. map each sentence to its word count (spaces + 1)
+        // 3. get the max
+        sentences
+            .into_iter()
+            .map(|sentence| sentence.matches(" ").count() + 1)
+            .max()
+            .unwrap() as i32
+    }
+}
