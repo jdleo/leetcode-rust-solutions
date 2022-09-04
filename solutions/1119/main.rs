a87c65d2c7af0f50eae7fd72ebd26796dad188a6
John Leonardo <j@jdleo.me>
2022-09-04 11:41:58 -0700

    done with 1119

---
+++ solutions/1119/main.rs
@@ -0,0 +1,11 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn remove_vowels(s: String) -> String {
+        // set of vowels
+        let vowels: HashSet<char> = ['a', 'e', 'i', 'o', 'u'].iter().cloned().collect();
+
+        // filter s by characters that are not vowels and collect to string
+        s.chars().filter(|c| !vowels.contains(c)).collect()
+    }
+}
