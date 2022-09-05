c68930bbd41d076822edf487f513586a1d0b8463
John Leonardo <j@jdleo.me>
2022-09-05 15:10:39 -0700

    finished problem 2176

--- solutions/src/problem_2176.rs
+++ solutions/src/problem_2176.rs
@@ -1,1 +1,19 @@
 struct Solution;
+
+impl Solution {
+    pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let mut result: i32 = 0;
+
+        // go through all pairs bruteforce
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                // check conditions provided by problem
+                if nums[i] == nums[j] && (i * j) % k as usize == 0 {
+                    result += 1
+                }
+            }
+        }
+
+        result
+    }
+}
