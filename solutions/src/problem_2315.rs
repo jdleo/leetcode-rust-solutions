93e764405037ceaed65b2d878f779194df8d7393
John Leonardo <j@jdleo.me>
2022-09-05 12:02:47 -0700

    finished problem 2315

--- solutions/src/problem_2315.rs
+++ solutions/src/problem_2315.rs
@@ -1,1 +1,24 @@
 struct Solution;
+
+impl Solution {
+    pub fn count_asterisks(s: String) -> i32 {
+        let mut result: i32 = 0;
+        let mut bar: u8 = 1;
+
+        // go through each character
+        for c in s.chars() {
+            // check if bar first
+            if c == '|' {
+                // toggle
+                bar ^= 1;
+            } else if c == '*' {
+                // only sum if bar is toggled on
+                if bar == 1 {
+                    result += 1
+                }
+            }
+        }
+
+        result
+    }
+}
