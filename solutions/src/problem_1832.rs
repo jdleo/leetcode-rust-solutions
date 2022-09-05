3a6c68e5ef25810c1cf453779d641266f4ed8fab
John Leonardo <j@jdleo.me>
2022-09-05 14:26:41 -0700

    finished problem 1832

--- solutions/src/problem_1832.rs
+++ solutions/src/problem_1832.rs
@@ -1,1 +1,10 @@
 struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn check_if_pangram(sentence: String) -> bool {
+        // collect into hashset and check if all 26 characters are used
+        sentence.chars().collect::<HashSet<char>>().len() == 26
+    }
+}
