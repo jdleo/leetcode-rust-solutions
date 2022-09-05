54bdb37163c924f7eaa0834e84944dc1734fbc68
John Leonardo <j@jdleo.me>
2022-09-05 11:48:41 -0700

    finished problem 1684

--- solutions/src/problem_1684.rs
+++ solutions/src/problem_1684.rs
@@ -1,1 +1,16 @@
 struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn count_consistent_strings(allowed: String, words: Vec<String>) -> i32 {
+        // collect allowed characters to set
+        let allowed_set: HashSet<char> = allowed.chars().collect();
+
+        // sum words that only have allowed characters
+        words
+            .iter()
+            .filter(|word| word.chars().all(|c| allowed_set.contains(&c)))
+            .count() as i32
+    }
+}
