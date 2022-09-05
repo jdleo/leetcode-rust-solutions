32a82b72ef24b942ceaa27bc7632bf18ad7e5822
John Leonardo <j@jdleo.me>
2022-09-05 16:23:16 -0700

    finished problem 647

--- solutions/src/problem_647.rs
+++ solutions/src/problem_647.rs
@@ -1,1 +1,25 @@
 struct Solution;
+
+impl Solution {
+    pub fn count_substrings(s: String) -> i32 {
+        // sum and expand at each index (counting for odd and even length ones)
+        (0..s.len()).fold(0, |acc, index| {
+            acc + Self::expand(s.as_bytes(), index as i32, index)
+                + Self::expand(s.as_bytes(), index as i32, index + 1)
+        })
+    }
+
+    // helper method to count palindromes anchored at i,j
+    fn expand(s: &[u8], mut i: i32, mut j: usize) -> i32 {
+        let mut result: i32 = 0;
+
+        // keep expanding
+        while i >= 0 && j < s.len() && s[i as usize] == s[j] {
+            i -= 1;
+            j += 1;
+            result += 1;
+        }
+
+        result
+    }
+}
