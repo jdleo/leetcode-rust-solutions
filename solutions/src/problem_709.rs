d33b40a2a75c1f6f421e347783b25d5301a10492
John Leonardo <j@jdleo.me>
2022-09-05 11:57:54 -0700

    finished problem 709

--- solutions/src/problem_709.rs
+++ solutions/src/problem_709.rs
@@ -1,1 +1,16 @@
 struct Solution;
+
+impl Solution {
+    pub fn to_lower_case(s: String) -> String {
+        // map each character to lowercase, only if uppercase, otherwise take the char
+        s.chars()
+            .map(|c| {
+                if 'A' <= c && c <= 'Z' {
+                    (c as u8 + 32) as char
+                } else {
+                    c
+                }
+            })
+            .collect()
+    }
+}
