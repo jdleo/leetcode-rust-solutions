38cea5430b5cce5c7c2cf4e72334a538d7a562ff
John Leonardo <j@jdleo.me>
2022-09-05 12:08:45 -0700

    finished problem 1816

--- solutions/src/problem_1816.rs
+++ solutions/src/problem_1816.rs
@@ -1,1 +1,25 @@
 struct Solution;
+
+impl Solution {
+    pub fn truncate_sentence(s: String, k: i32) -> String {
+        let mut result: String = String::new();
+        let mut space_count: i32 = 0;
+
+        for c in s.chars() {
+            // check if space
+            if c == ' ' {
+                // increment space count and check if we've already taken k words
+                space_count += 1;
+
+                if (space_count == k) {
+                    break;
+                }
+            }
+
+            // add character
+            result.push(c);
+        }
+
+        result
+    }
+}
