b895e1aaed8837e09c9582e7d2bb753191aaa9a9
John Leonardo <j@jdleo.me>
2022-09-05 11:31:00 -0700

    finished problem 2037

--- solutions/src/problem_2037.rs
+++ solutions/src/problem_2037.rs
@@ -1,1 +1,15 @@
 struct Solution;
+
+impl Solution {
+    pub fn min_moves_to_seat(mut seats: Vec<i32>, mut students: Vec<i32>) -> i32 {
+        // sort seats and students
+        seats.sort_unstable_by(|a, b| a.cmp(b));
+        students.sort_unstable_by(|a, b| a.cmp(b));
+
+        // sum differences (moves)
+        students
+            .iter()
+            .zip(seats)
+            .fold(0, |acc, (student, seat)| acc + (student - seat).abs())
+    }
+}
