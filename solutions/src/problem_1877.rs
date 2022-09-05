344b1bbd8208445dd72e396cf95879ccf29ca8e1
John Leonardo <j@jdleo.me>
2022-09-05 15:02:44 -0700

    finished problem 1877

--- solutions/src/problem_1877.rs
+++ solutions/src/problem_1877.rs
@@ -1,1 +1,23 @@
 struct Solution;
+
+impl Solution {
+    pub fn min_pair_sum(mut nums: Vec<i32>) -> i32 {
+        let mut result: i32 = 0;
+
+        // sort nums (so we can minimize all sums)
+        nums.sort_unstable();
+
+        // go up to half
+        for index in 0..nums.len() / 2 {
+            // smallest and biggest
+            let sum = nums[index] + nums[nums.len() - index - 1];
+
+            // check if new max
+            if sum > result {
+                result = sum;
+            }
+        }
+
+        result
+    }
+}
