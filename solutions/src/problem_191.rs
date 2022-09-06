8a1fed0b1b7a627f9a4f346b02728666bd40c820
John Leonardo <j@jdleo.me>
2022-09-05 17:18:21 -0700

    finished problem 191

--- solutions/src/problem_191.rs
+++ solutions/src/problem_191.rs
@@ -1,1 +1,17 @@
 struct Solution;
+
+impl Solution {
+    pub fn hammingWeight(n: u32) -> i32 {
+        let (mut n, mut result) = (n, 0);
+
+        // go until n is 0
+        while n > 0 {
+            // count this bit (if set)
+            result += n & 1;
+            // shift to get next bit
+            n >>= 1;
+        }
+
+        result as i32
+    }
+}
