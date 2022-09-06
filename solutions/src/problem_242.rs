c4b717e90f08dd2ad68edf5a802c287fa7ad0e62
John Leonardo <j@jdleo.me>
2022-09-05 17:46:19 -0700

    finished problem 242

--- solutions/src/problem_242.rs
+++ solutions/src/problem_242.rs
@@ -1,1 +1,22 @@
 struct Solution;
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+
+        // vector for character counts
+        let mut counts: Vec<u16> = vec![0; 26];
+
+        // count characters
+        for (a, b) in s.chars().zip(t.chars()) {
+            // maintain balance
+            counts[a as usize - 97] += 1;
+            counts[b as usize - 97] -= 1;
+        }
+
+        // all counts should be 0 (balanced)
+        counts.into_iter().all(|c| c == 0)
+    }
+}
