711418cc4a73a49e876a57acd6dcee3a13c158c5
John Leonardo <j@jdleo.me>
2022-09-06 19:51:12 -0700

    finished problem 1967

--- solutions/src/problem_1967.rs
+++ solutions/src/problem_1967.rs
@@ -1,1 +1,15 @@
 struct Solution;
+
+impl Solution {
+    pub fn num_of_strings(patterns: Vec<String>, word: String) -> i32 {
+        /*
+        1. turn into iterator
+        2. filter each pattern which exist inside word
+        3. count them
+        */
+        patterns
+            .into_iter()
+            .filter(|pattern| word.contains(pattern))
+            .count() as i32
+    }
+}
