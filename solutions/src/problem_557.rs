9eddf1047d7a5322534436e1a2bb288fa2d213e9
John Leonardo <j@jdleo.me>
2022-09-06 19:19:14 -0700

    finished problem 557

--- solutions/src/problem_557.rs
+++ solutions/src/problem_557.rs
@@ -1,1 +1,16 @@
 struct Solution;
+
+impl Solution {
+    pub fn reverse_words(s: String) -> String {
+        /*
+        1. split by spaces
+        2. map each word to its reverse
+        3. collect into string vector
+        4. join back by space
+        */
+        s.split(' ')
+            .map(|word| word.chars().rev().collect())
+            .collect::<Vec<String>>()
+            .join(" ")
+    }
+}
