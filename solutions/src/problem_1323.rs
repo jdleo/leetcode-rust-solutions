b3958f0a5e2f9e4472a25d3a0b6fd7a692041d11
John Leonardo <j@jdleo.me>
2022-09-07 18:05:12 -0700

    finished problem 1323

--- solutions/src/problem_1323.rs
+++ solutions/src/problem_1323.rs
@@ -1,1 +1,16 @@
 struct Solution;
+
+impl Solution {
+    pub fn maximum69_number(num: i32) -> i32 {
+        /*
+        cast to string
+        replace first 6 to a 9
+        parse back to i32
+        force unwrap
+        */
+        num.to_string()
+            .replacen("6", "9", 1)
+            .parse::<i32>()
+            .unwrap()
+    }
+}
