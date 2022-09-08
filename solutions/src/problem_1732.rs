f8e8ad1e1afe498d541b6a1873be472637977aaa
John Leonardo <j@jdleo.me>
2022-09-07 18:25:13 -0700

    finished problem 1732

--- solutions/src/problem_1732.rs
+++ solutions/src/problem_1732.rs
@@ -1,1 +1,17 @@
 struct Solution;
+
+impl Solution {
+    pub fn largest_altitude(gain: Vec<i32>) -> i32 {
+        let mut result = 0i32;
+        let mut height = 0i32;
+
+        // climb
+        for climb in gain.into_iter() {
+            // set new max
+            height += climb;
+            result = if height > result { height } else { result }
+        }
+
+        result
+    }
+}
