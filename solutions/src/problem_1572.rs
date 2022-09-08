8c4e6cdf0af89bfa5b1fc95e5682131e04cbc809
John Leonardo <j@jdleo.me>
2022-09-07 17:21:42 -0700

    finished problem 1572

--- solutions/src/problem_1572.rs
+++ solutions/src/problem_1572.rs
@@ -1,1 +1,19 @@
 struct Solution;
+
+impl Solution {
+    pub fn diagonal_sum(mat: Vec<Vec<i32>>) -> i32 {
+        let (mut sum, n) = (0, mat.len());
+
+        for i in 0..n {
+            // add from top left, and top right
+            sum += mat[i][i] + mat[i][n - i - 1];
+        }
+
+        // if n was odd, we double added the middle one
+        if n & 1 == 1 {
+            sum -= mat[n / 2][n / 2]
+        }
+
+        sum
+    }
+}
