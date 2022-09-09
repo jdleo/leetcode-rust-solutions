607e449c21eefc52c622fdc0089f9c88667cf1de
John Leonardo <j@jdleo.me>
2022-09-08 19:37:56 -0700

    finished problem 2185

--- solutions/src/problem_2185.rs
+++ solutions/src/problem_2185.rs
@@ -1,1 +1,10 @@
 struct Solution;
+
+impl Solution {
+    pub fn prefix_count(words: Vec<String>, pref: String) -> i32 {
+        words
+            .into_iter()
+            .filter(|word| word.starts_with(&pref))
+            .count() as i32
+    }
+}
