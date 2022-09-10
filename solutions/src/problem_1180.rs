62c193b4ed2b0ac59a3808409fc41b3539133ae7
John Leonardo <j@jdleo.me>
2022-09-09 19:48:47 -0700

    finished problem 1180

--- solutions/src/problem_1180.rs
+++ solutions/src/problem_1180.rs
@@ -1,1 +1,23 @@
 struct Solution;
+
+impl Solution {
+    pub fn count_letters(s: String) -> i32 {
+        let (mut result, mut streak, s) = (0i32, 1i32, s.as_bytes());
+
+        // go through string
+        for index in 1..s.len() {
+            if s[index] != s[index - 1] {
+                // add current streak (all substrings using math combinatorics)
+                // and reset streak
+                result += (streak * (streak + 1)) >> 1;
+                streak = 0;
+            }
+
+            // streak continues no matter what
+            streak += 1;
+        }
+
+        // add whatever streak is left
+        result + ((streak * (streak + 1)) >> 1)
+    }
+}
