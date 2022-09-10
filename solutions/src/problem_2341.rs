d03db7f116b9c1a1980bc32df1b98e7c0d33b800
John Leonardo <j@jdleo.me>
2022-09-10 16:41:52 -0700

    finished problem 2341

--- solutions/src/problem_2341.rs
+++ solutions/src/problem_2341.rs
@@ -1,1 +1,23 @@
 struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn number_of_pairs(nums: Vec<i32>) -> Vec<i32> {
+        let mut result: Vec<i32> = Vec::from([0, 0]);
+        // count frequencies of all numbers
+        let mut counts: HashMap<i32, i32> = HashMap::new();
+        for num in nums.into_iter() {
+            *counts.entry(num).or_insert(0) += 1;
+        }
+
+        // go through entries
+        for (_, count) in counts.into_iter() {
+            // add pairs and leftovers (lonely ones) to result
+            result[0] += count / 2;
+            result[1] += count & 1;
+        }
+
+        result
+    }
+}
