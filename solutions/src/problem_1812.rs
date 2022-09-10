4d931245e1042abb9fbed243399972e54b226879
John Leonardo <j@jdleo.me>
2022-09-10 16:29:40 -0700

    finished problem 1812

--- solutions/src/problem_1812.rs
+++ solutions/src/problem_1812.rs
@@ -1,1 +1,11 @@
 struct Solution;
+
+impl Solution {
+    pub fn square_is_white(coordinates: String) -> bool {
+        // get i and j
+        let (i, j) = (coordinates.as_bytes()[0], coordinates.as_bytes()[1]);
+
+        // flatten, and check if odd
+        (i + j) & 1 == 1
+    }
+}
