fce4be0d6bfe143f311e130d4ec7acd4f0bf3525
John Leonardo <j@jdleo.me>
2022-09-09 20:41:15 -0700

    finished problem 2000

--- solutions/src/problem_2000.rs
+++ solutions/src/problem_2000.rs
@@ -1,1 +1,22 @@
 struct Solution;
+
+impl Solution {
+    pub fn reverse_prefix(word: String, ch: char) -> String {
+        let (mut result, mut did_reverse) = (String::new(), false);
+
+        // walk through string
+        for chr in word.chars() {
+            // add this character
+            result.push(chr);
+
+            // check if this is character and have not reversed
+            if chr == ch && !did_reverse {
+                // reverse result as is and just continue
+                result = result.chars().rev().collect();
+                did_reverse = true;
+            }
+        }
+
+        result
+    }
+}
