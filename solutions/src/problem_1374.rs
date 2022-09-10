dc0ccacbfa9d1f5503f1ae569e757d9d4a23bfd7
John Leonardo <j@jdleo.me>
2022-09-10 15:52:29 -0700

    finished problem 1374

--- solutions/src/problem_1374.rs
+++ solutions/src/problem_1374.rs
@@ -1,1 +1,18 @@
 struct Solution;
+
+impl Solution {
+    pub fn generate_the_string(n: i32) -> String {
+        let mut n: usize = n as usize;
+        let mut result = String::new();
+
+        // add single "a" if n is even
+        if n & 1 == 0 {
+            result.push('a');
+            n -= 1;
+        }
+
+        // add n b's
+        result.push_str(String::from_utf8(vec![b'b'; n]).as_ref().unwrap());
+        result
+    }
+}
