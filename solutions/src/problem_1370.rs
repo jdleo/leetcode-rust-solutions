ae1618fa5f9c6900e04232ac68b1b8a106a486c5
John Leonardo <j@jdleo.me>
2022-09-10 15:34:37 -0700

    finished problem 1370

--- solutions/src/problem_1370.rs
+++ solutions/src/problem_1370.rs
@@ -1,1 +1,31 @@
+use std::borrow::Borrow;
+
 struct Solution;
+
+impl Solution {
+    pub fn sort_string(s: String) -> String {
+        let mut s = s;
+        let mut result = String::new();
+        let mut alphabet = String::from("abcdefghijklmnopqrstuvwxyz");
+
+        // go until string is empty and accounted for
+        while !s.is_empty() {
+            // go through alphabet in sequential order
+            for chr in alphabet.clone().chars() {
+                if s.contains(chr) {
+                    // smallest/biggest character and append it
+                    result.push(chr);
+                    s = s.replacen(chr, "", 1);
+                } else {
+                    // we should remove this to save runtime
+                    alphabet = alphabet.replace(chr, "");
+                }
+            }
+
+            // reverse alphabet because next iteration will be opposite dir
+            alphabet = alphabet.chars().rev().collect();
+        }
+
+        result
+    }
+}
