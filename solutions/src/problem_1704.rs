f5f37386a480e44ddad2230689fe45cba32ff1ef
John Leonardo <j@jdleo.me>
2022-09-10 15:42:55 -0700

    finished problem 1704

--- solutions/src/problem_1704.rs
+++ solutions/src/problem_1704.rs
@@ -1,1 +1,22 @@
 struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn halves_are_alike(s: String) -> bool {
+        // set of vowels and vowel count "balance"
+        let vowels: HashSet<char> = String::from("aeiouAEIOU").chars().collect();
+        let mut balance = 0i16;
+
+        for (index, chr) in s.chars().enumerate() {
+            // check if vowel
+            if vowels.contains(&chr) {
+                // if first half, increment, if second half, decrement
+                balance += if index >= s.len() / 2 { 1 } else { -1 }
+            }
+        }
+
+        // string halves should be equally balanced with vowels
+        balance == 0
+    }
+}
