ee1ab57404f2f5471a8a8762d08d461a0bdb9c88
John Leonardo <j@jdleo.me>
2022-09-11 12:52:31 -0700

    finished problem 2119

--- solutions/src/problem_2119.rs
+++ solutions/src/problem_2119.rs
@@ -1,1 +1,27 @@
 struct Solution;
+
+impl Solution {
+    pub fn is_same_after_reversals(num: i32) -> bool {
+        let mut current = num;
+        let mut result = 0i32;
+
+        // reverse once
+        while current > 0 {
+            result *= 10;
+            result += current % 10;
+            current /= 10;
+        }
+
+        // reverse again
+        current = result;
+        result = 0;
+        while current > 0 {
+            result *= 10;
+            result += current % 10;
+            current /= 10;
+        }
+
+        // double reverse == num
+        result == num
+    }
+}
