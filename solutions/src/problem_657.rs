accb4455998de9b38b78331a22348c0dc96a677b
John Leonardo <j@jdleo.me>
2022-09-10 23:35:07 -0700

    finished problem 657

--- solutions/src/problem_657.rs
+++ solutions/src/problem_657.rs
@@ -1,1 +1,21 @@
 struct Solution;
+
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        // x,y coordinates starting at (0,0)
+        let (mut x, mut y) = (0, 0);
+
+        // go thru moves and map UDLR to moves
+        for chr in moves.chars() {
+            match chr {
+                'U' => y += 1,
+                'D' => y -= 1,
+                'L' => x -= 1,
+                _ => x += 1,
+            }
+        }
+
+        // make sure back at (0,0)
+        (x, y) == (0, 0)
+    }
+}
