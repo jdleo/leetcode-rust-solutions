692072aa55e810a2e0ceb4dc3a691d037dee51e4
John Leonardo <j@jdleo.me>
2022-09-11 13:09:11 -0700

    finished problem 905

--- solutions/src/problem_905.rs
+++ solutions/src/problem_905.rs
@@ -1,1 +1,33 @@
 struct Solution;
+
+impl Solution {
+    pub fn sort_array_by_parity(nums: Vec<i32>) -> Vec<i32> {
+        // shadow nums and create two pointers
+        let mut nums = nums;
+        let (mut left, mut right) = (0usize, nums.len() - 1);
+
+        // go until meet in middle
+        while left < right {
+            // 4 scenarios:
+            // 0: left even, right even: move left forward
+            // 1: left odd, right odd: move right forward
+            // 2: left even, right odd: move left, right forward
+            // 3: left odd, right even: swap, move left, right forward
+            match (nums[left] & 1, nums[right] & 1) {
+                (0, 0) => left += 1,
+                (1, 1) => right -= 1,
+                (0, 1) => {
+                    left += 1;
+                    right -= 1;
+                }
+                (_, _) => {
+                    nums.swap(left, right);
+                    left += 1;
+                    right -= 1;
+                }
+            }
+        }
+
+        nums
+    }
+}
