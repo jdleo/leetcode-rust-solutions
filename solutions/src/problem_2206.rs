5934e17f248ad327473ba0436467825ea5b8b38e
John Leonardo <j@jdleo.me>
2022-09-11 15:34:54 -0700

    finished problem 2206

--- solutions/src/problem_2206.rs
+++ solutions/src/problem_2206.rs
@@ -1,1 +1,16 @@
 struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn divide_array(nums: Vec<i32>) -> bool {
+        // count frequencies
+        let mut counts: HashMap<i32, u16> = HashMap::new();
+        for num in nums.into_iter() {
+            *counts.entry(num).or_insert(0) += 1;
+        }
+
+        // all values must be even (evenly paired)
+        counts.values().all(|value| value & 1 == 0)
+    }
+}
