832375c42670de3c5bac9164a7f37389d065e518
John Leonardo <j@jdleo.me>
2022-09-10 23:01:49 -0700

    finished problem 344

--- solutions/src/problem_344.rs
+++ solutions/src/problem_344.rs
@@ -1,1 +1,17 @@
 struct Solution;
+
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        let (mut left, mut right) = (0usize, s.len() - 1);
+
+        // go until meet in the middle
+        while left < right {
+            // swap
+            s.swap(left, right);
+
+            // pinch to middle
+            left += 1;
+            right -= 1;
+        }
+    }
+}
