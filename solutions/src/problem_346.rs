16f64b24d0cbda7cc9520a5493f1a0137fe252ba
John Leonardo <j@jdleo.me>
2022-09-10 17:15:49 -0700

    finished problem 346

--- solutions/src/problem_346.rs
+++ solutions/src/problem_346.rs
@@ -1,1 +1,32 @@
-struct Solution;
+use std::collections::VecDeque;
+
+struct MovingAverage {
+    queue: VecDeque<i32>,
+    size: i32,
+    sum: i32,
+}
+
+impl MovingAverage {
+    fn new(size: i32) -> Self {
+        Self {
+            queue: VecDeque::new(),
+            size,
+            sum: 0,
+        }
+    }
+
+    fn next(&mut self, val: i32) -> f64 {
+        // push this number to queue and add sum
+        self.queue.push_back(val);
+        self.sum += val;
+
+        // check if we're over max capacity
+        if self.queue.len() as i32 > self.size {
+            // popleft and subtract from sum
+            self.sum -= self.queue.pop_front().unwrap();
+        }
+
+        // average
+        self.sum as f64 / self.queue.len() as f64
+    }
+}
