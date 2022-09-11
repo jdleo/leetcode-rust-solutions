7b4ddf49efee67330801dfdcfad8886769012e20
John Leonardo <j@jdleo.me>
2022-09-10 17:07:18 -0700

    finished problem 1295

--- solutions/src/problem_1295.rs
+++ solutions/src/problem_1295.rs
@@ -1,1 +1,9 @@
 struct Solution;
+
+impl Solution {
+    pub fn find_numbers(nums: Vec<i32>) -> i32 {
+        nums.into_iter()
+            .filter(|num| num.to_string().len() & 1 == 0)
+            .count() as i32
+    }
+}
