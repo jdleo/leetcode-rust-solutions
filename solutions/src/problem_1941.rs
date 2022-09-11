dd5f4b9fbf2e5836919b1b14518c83b17134ed0d
John Leonardo <j@jdleo.me>
2022-09-10 17:28:47 -0700

    finished problem 1941

--- solutions/src/problem_1941.rs
+++ solutions/src/problem_1941.rs
@@ -1,1 +1,17 @@
 struct Solution;
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn are_occurrences_equal(s: String) -> bool {
+        // first, count all occurrences
+        let mut counts: HashMap<char, i32> = HashMap::new();
+        for chr in s.chars() {
+            *counts.entry(chr).or_insert(0) += 1;
+        }
+
+        // check if all occurrences are equal
+        counts.values().collect::<HashSet<&i32>>().len() == 1
+    }
+}
