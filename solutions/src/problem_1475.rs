48063bfb6876365589776cb6784d5ffb5e035d14
John Leonardo <j@jdleo.me>
2022-09-11 14:26:44 -0700

    finished problem 1475

--- solutions/src/problem_1475.rs
+++ solutions/src/problem_1475.rs
@@ -1,1 +1,24 @@
 struct Solution;
+
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn final_prices(prices: Vec<i32>) -> Vec<i32> {
+        let mut prices: Vec<i32> = prices;
+        let mut stack: VecDeque<usize> = VecDeque::new();
+
+        // walk thru prices
+        for i in 0..prices.len() {
+            // see if this current price can be applied as a discount to any we seen
+            while !stack.is_empty() && prices[i] <= prices[*stack.back().unwrap()] {
+                // use this one and pop it
+                prices[stack.pop_back().unwrap()] -= prices[i];
+            }
+
+            // add this to stack to have discount applied to it later
+            stack.push_back(i);
+        }
+
+        prices
+    }
+}
