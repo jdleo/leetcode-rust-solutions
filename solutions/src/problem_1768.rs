462f15edc0024e96a6e1b5148d6aa44469c1cc51
John Leonardo <j@jdleo.me>
2022-09-10 23:26:38 -0700

    finished problem 1768

--- solutions/src/problem_1768.rs
+++ solutions/src/problem_1768.rs
@@ -1,1 +1,25 @@
 struct Solution;
+
+impl Solution {
+    pub fn merge_alternately(word1: String, word2: String) -> String {
+        let (word1, word2) = (word1.as_bytes(), word2.as_bytes());
+        let (mut i, mut j) = (0usize, 0usize);
+        let mut result = String::new();
+
+        // go while either still have characters to merge
+        while i < word1.len() || j < word2.len() {
+            // merge
+            if i < word1.len() {
+                result.push(word1[i] as char);
+                i += 1;
+            }
+
+            if j < word2.len() {
+                result.push(word2[j] as char);
+                j += 1;
+            }
+        }
+
+        result
+    }
+}
