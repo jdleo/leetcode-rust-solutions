ef48006746503bef90ef7d56137bbeb28d5b28fb
John Leonardo <j@jdleo.me>
2022-09-11 19:11:15 -0700

    finished problem 461

--- solutions/src/problem_461.rs
+++ solutions/src/problem_461.rs
@@ -1,1 +1,18 @@
 struct Solution;
+
+impl Solution {
+    pub fn hamming_distance(x: i32, y: i32) -> i32 {
+        let (mut x, mut y, mut result) = (x, y, 0);
+
+        // go until x and y are 0 (still bits to compare)
+        while x != 0 || y != 0 {
+            // xor the rightmost bits, if they differ, thatll be + 1
+            // then right shift them both to get the next bits
+            result += ((x & 1) ^ (y & 1));
+            x >>= 1;
+            y >>= 1;
+        }
+
+        result
+    }
+}
