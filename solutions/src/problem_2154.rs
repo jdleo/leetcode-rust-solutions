27be00e5e1808f27a0e351dbc1eec56f0c24e5ae
John Leonardo <j@jdleo.me>
2022-09-12 19:10:27 -0700

    finished problem 2154

--- solutions/src/problem_2154.rs
+++ solutions/src/problem_2154.rs
@@ -1,1 +1,17 @@
 struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn find_final_value(nums: Vec<i32>, original: i32) -> i32 {
+        let mut original = original;
+
+        // put all nums in set and keep doubling until not in seen
+        let seen: HashSet<i32> = nums.into_iter().collect();
+        while seen.contains(&original) {
+            original <<= 1;
+        }
+
+        original
+    }
+}
