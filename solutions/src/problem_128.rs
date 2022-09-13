c99b12db0bd70035b5681239aa8c4ada6da20426
John Leonardo <j@jdleo.me>
2022-09-12 20:47:38 -0700

    finished problem 128

--- solutions/src/problem_128.rs
+++ solutions/src/problem_128.rs
@@ -1,1 +1,33 @@
 struct Solution;
+
+use std::cmp::max;
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        let mut result = 0i32;
+
+        // first, collect all nums to set for o(1) lookups
+        let seen: HashSet<i32> = nums.clone().into_iter().collect::<HashSet<i32>>();
+
+        // go through set
+        for num in nums.into_iter() {
+            // check if this is first in sequence (left not found)
+            if !seen.contains(&(num - 1)) {
+                let mut current = num;
+                let mut count = 0i32;
+
+                // keep counting as so long as we can continue it from seen
+                while seen.contains(&current) {
+                    count += 1;
+                    current += 1;
+                }
+
+                // set max subsequence
+                result = max(result, count);
+            }
+        }
+
+        result
+    }
+}
