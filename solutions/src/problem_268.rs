60649f072d0d35656360153e20f31556d5d66da0
John Leonardo <j@jdleo.me>
2022-09-12 18:32:40 -0700

    finished problem 268

--- solutions/src/problem_268.rs
+++ solutions/src/problem_268.rs
@@ -1,1 +1,12 @@
 struct Solution;
+
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        // all numbers should be paired up with indices, so the missing one is whats left
+        // after xoring them all because xoring pairs = 0 and 0 xor x = x
+        nums.iter()
+            .enumerate()
+            .fold(0, |acc, (index, num)| acc ^ (index as i32) ^ num)
+            ^ (nums.len() as i32)
+    }
+}
