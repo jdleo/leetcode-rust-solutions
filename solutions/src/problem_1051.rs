c5bf958d991b44654255893b47551cebf016743b
John Leonardo <j@jdleo.me>
2022-09-12 18:45:14 -0700

    finished problem 1051

--- solutions/src/problem_1051.rs
+++ solutions/src/problem_1051.rs
@@ -1,1 +1,14 @@
 struct Solution;
+
+impl Solution {
+    pub fn height_checker(heights: Vec<i32>) -> i32 {
+        // clone and sort
+        let mut expected = heights.clone();
+        expected.sort_unstable();
+
+        // count all where different
+        (0..heights.len())
+            .into_iter()
+            .fold(0, |acc, i| acc + (heights[i] != expected[i]) as i32)
+    }
+}
