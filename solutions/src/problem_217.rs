f8bd366a38643234e2fde0b5f104fe29e7283bbd
John Leonardo <j@jdleo.me>
2022-09-12 18:37:20 -0700

    finished problem 217

--- solutions/src/problem_217.rs
+++ solutions/src/problem_217.rs
@@ -1,1 +1,21 @@
 struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        // to store history
+        let mut seen: HashSet<i32> = HashSet::new();
+
+        // if at any point we already seen, return true
+        for num in nums.iter() {
+            if seen.contains(num) {
+                return true;
+            }
+
+            seen.insert(*num);
+        }
+
+        false
+    }
+}
