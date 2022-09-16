887af1747a047f9836e36c7c2ef8292dfa001fac
John Leonardo <j@jdleo.me>
2022-09-15 22:39:40 -0700

    finished problem 2248

--- solutions/src/problem_2248.rs
+++ solutions/src/problem_2248.rs
@@ -1,1 +1,26 @@
 struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
+        // hashmap for counting frequencies
+        let n = nums.len() as i32;
+        let mut counts: HashMap<i32, i32> = HashMap::new();
+        let mut result: Vec<i32> = Vec::new();
+
+        // go through all nums in each array
+        for row in nums.into_iter() {
+            for num in row.into_iter() {
+                // increment
+                *counts.entry(num).or_insert(0) += 1;
+                // check if in ALL arrays
+                if *counts.get(&num).unwrap() == n {
+                    result.push(num);
+                }
+            }
+        }
+        result.sort_unstable();
+        result
+    }
+}
