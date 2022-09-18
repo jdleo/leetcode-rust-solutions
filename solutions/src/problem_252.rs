625f879efe264581ba7954bb672ee039d8857fa2
John Leonardo <j@jdleo.me>
2022-09-17 17:52:41 -0700

    finished problem 252

--- solutions/src/problem_252.rs
+++ solutions/src/problem_252.rs
@@ -1,2 +1,18 @@
 struct Solution;
-struct Solution;
+
+impl Solution {
+    pub fn can_attend_meetings(intervals: Vec<Vec<i32>>) -> bool {
+        // shadow and sort by start times
+        let mut intervals = intervals;
+        intervals.sort_unstable_by_key(|interval| interval[0]);
+
+        // if any have collisions, can't attend all
+        for index in 1..intervals.len() {
+            if intervals[index][0] < intervals[index - 1][1] {
+                return false;
+            }
+        }
+
+        true
+    }
+}
