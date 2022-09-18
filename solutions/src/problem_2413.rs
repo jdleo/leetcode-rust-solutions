9491200693c34a932ed899a2f5fcdd811d9292a2
John Leonardo <j@jdleo.me>
2022-09-18 11:05:08 -0700

    finished problem 2413

--- solutions/src/problem_2413.rs
+++ solutions/src/problem_2413.rs
@@ -1,1 +1,12 @@
 struct Solution;
+
+impl Solution {
+    pub fn smallest_even_multiple(n: i32) -> i32 {
+        // if it's even, return it because n is divisor of n
+        // if not, just multiply by 2
+        match n & 1 {
+            1 => n * 2,
+            _ => n,
+        }
+    }
+}
