c05035029241449677ddf581fd2825635e5af932
John Leonardo <j@jdleo.me>
2022-09-17 19:12:16 -0700

    finished problem 190

--- solutions/src/problem_190.rs
+++ solutions/src/problem_190.rs
@@ -1,1 +1,24 @@
 struct Solution;
+
+impl Solution {
+    pub fn reverse_bits(x: u32) -> u32 {
+        // unsigned result starting at 0
+        let (mut result, mut x) = (0u32, x);
+
+        // go all potential 32 unsigned bits
+        for _ in 0..32 {
+            // get rightmost bit
+            let rightmost = x & 1;
+
+            // left shift result to make room for new bit
+            // and right shift x to keep taking bits
+            result <<= 1;
+            x >>= 1;
+
+            // add this bit to it
+            result |= rightmost;
+        }
+
+        result
+    }
+}
