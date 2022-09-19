47751542052f8218c0b8d7fe7a734d5b7e92bcba
John Leonardo <j@jdleo.me>
2022-09-18 17:15:56 -0700

    finished problem 46

--- solutions/src/problem_46.rs
+++ solutions/src/problem_46.rs
@@ -1,1 +1,32 @@
 struct Solution;
+
+impl Solution {
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        // mutable result
+        let mut result: Vec<Vec<i32>> = Vec::new();
+        // backtrack and fill result with mutable reference
+        Self::backtrack(&nums, &mut result, &mut Vec::new());
+        result
+    }
+
+    fn backtrack(nums: &Vec<i32>, result: &mut Vec<Vec<i32>>, path: &mut Vec<i32>) {
+        // check if path is a permutation
+        if path.len() == nums.len() {
+            // push copy of path
+            (*result).push(path.clone());
+        } else {
+            // go thru all candidates
+            for num in nums.into_iter() {
+                // make sure not already picked
+                if path.contains(num) {
+                    continue;
+                }
+
+                // backtrack
+                (*path).push(*num);
+                Self::backtrack(nums, result, path);
+                (*path).pop();
+            }
+        }
+    }
+}
