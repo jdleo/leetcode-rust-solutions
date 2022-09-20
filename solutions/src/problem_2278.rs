184a299b6a3b64237026fa9885450d3577547d63
John Leonardo <j@jdleo.me>
2022-09-19 17:51:36 -0700

    finished problem 2278

--- solutions/src/problem_2278.rs
+++ solutions/src/problem_2278.rs
@@ -1,1 +1,15 @@
 struct Solution;
+
+impl Solution {
+    pub fn percentage_letter(s: String, letter: char) -> i32 {
+        let mut count: i32 = 0;
+
+        // count characters that match
+        for chr in s.chars() {
+            count += (chr == letter) as i32;
+        }
+
+        // percentage, as 100th, rounded down
+        (count * 100) / (s.len() as i32)
+    }
+}
